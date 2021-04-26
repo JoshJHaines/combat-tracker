87fe5594dc6fc2350576adfd23a3947749755740
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-26 15:36:37 -0400

    added listener for intitiative submit button

---
+++ jsFiles/main.js
@@ -0,0 +1,19 @@
+/*************
+ * SELECTORS *
+ *************/
+
+const initiativeButton = document.getElementById("addToInit")
+
+
+/******************
+ * EVENT LISTENER *
+ ******************/
+
+initiativeButton.addEventListener("click", addToInitiative)
+
+/********************
+ * HELPER FUNCTIONS *
+ ********************/
+function addToInitiative (){
+    console.log("Add Character to Initiative Tracker")
+}
