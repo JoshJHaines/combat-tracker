7ecd4935b22318cd6e3d79489a9cad8322fd9843
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-26 18:50:46 -0400

    idk...made these changes a few hours ago

--- jsFiles/main.js
+++ jsFiles/main.js
@@ -8,18 +8,32 @@ const characterType = document.getElementById("chartype")
 const initiative = document.getElementById("initiative")
 
 
+/**************************
+ * OTHER GLOBAL VARIABLES *
+ **************************/
+ let initiativeList = []
+
 /******************
  * EVENT LISTENER *
  ******************/
 
-initiativeButton.addEventListener("click", addToInitiative)
+initiativeButton.addEventListener("click", addToInitiativeList)
 
 /********************
  * HELPER FUNCTIONS *
  ********************/
-function addToInitiative (){
-    console.log("Add Character to Initiative Tracker")
-    console.log("Character Name: ", characterName.value)
-    console.log("Character Type: ", characterType.value)
-    console.log("Initiative Roll: ", initiative.value)
+function addToInitiativeList (){
+            // console.log("Add Character to Initiative Tracker")
+            // console.log("Character Name: ", characterName.value)
+            // console.log("Character Type: ", characterType.value)
+            // console.log("Initiative Roll: ", initiative.value)
+//create an object with the input values in initiative tracker fields
+    let initiativeObj =  {}
+    initiativeObj.characterName = characterName.value;
+    initiativeObj.characterType = characterType.value;
+    initiativeObj.initiativeRoll = initiative.value;
+            // console.log(initiativeObj)
+//push the obj into global variable
+    initiativeList.push(initiativeObj)
+            console.log(initiativeList)
 }
