c228ab5317dca8750b3d7ed406e347c7dcaf8522
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-26 15:58:15 -0400

    able to accept all inputs for initiative via their values.

--- jsFiles/main.js
+++ jsFiles/main.js
@@ -3,6 +3,9 @@
  *************/
 
 const initiativeButton = document.getElementById("addToInit")
+const characterName = document.getElementById("characterName")
+const characterType = document.getElementById("chartype")
+const initiative = document.getElementById("initiative")
 
 
 /******************
@@ -16,4 +19,7 @@ initiativeButton.addEventListener("click", addToInitiative)
  ********************/
 function addToInitiative (){
     console.log("Add Character to Initiative Tracker")
+    console.log("Character Name: ", characterName.value)
+    console.log("Character Type: ", characterType.value)
+    console.log("Initiative Roll: ", initiative.value)
 }
