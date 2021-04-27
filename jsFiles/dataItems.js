22ee95910831e74bf5f4b516f8804a879b1f40e1
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-27 11:37:08 -0400

    added data types

--- jsFiles/dataItems.js
+++ jsFiles/dataItems.js
@@ -1,1 +1,5 @@
-const characterTypes = ["pc", "npc"];
+const CHARACTERTYPES = ["pc", "npc"];
+
+const ATTACKTYPES = ["melee", "ranged", "spell"];
+
+const DAMAGETYPES = ["acid", "bludgeoning", "cold", "fire", "force", "lightning", "necrotic", "piercing", "poison", "psychic", "radiant", "slashing", "thunder"];
