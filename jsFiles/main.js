f9f9f4ef3e0e4a4ffe48925e202c409fff4e8f33
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-27 14:21:08 -0400

    created a function to create dropdown lists from data sets

--- jsFiles/main.js
+++ jsFiles/main.js
@@ -6,11 +6,14 @@ const initiativeButton = document.getElementById("addToInit");
 const characterName = document.getElementById("characterName");
 const characterType = document.getElementById("chartype");
 const initiative = document.getElementById("initiative");
-const initTable = document.getElementById('initTable')
+const initTable = document.getElementById("initTable");
 const resetInitButton = document.getElementById("resetInit");
 
 /* DAMAGE TRACKER */
 const addDamageButton = document.getElementById("addToDmg");
+const atkTypeSelect = document.getElementById("atkType");
+const dmg1TypeSelect = document.getElementById("dmg1Type");
+const dmg2TypeSelect = document.getElementById("dmg2Type");
 
 /**************************
  * OTHER GLOBAL VARIABLES *
@@ -18,7 +21,7 @@ const addDamageButton = document.getElementById("addToDmg");
 let unOrderedInit = [];
 let orderedInit = [];
 
-let dmgList = []
+let dmgList = [];
 
 /******************
  * EVENT LISTENER *
@@ -49,7 +52,7 @@ function addToInitiativeList() {
 	//use function to order the initiative
 	orderInitiative(unOrderedInit);
 
-   // buildInitTable(orderedInit)
+	// buildInitTable(orderedInit)
 }
 
 function orderInitiative(obj) {
@@ -76,51 +79,68 @@ function resetInit() {
 	console.log("Ordered Init: ", orderedInit);
 }
 
-function buildInitTable(orderedInit){
-    // row = table.insertRow(0)
-    // nameCell = row.insertCell(0)
-    // charTypeCell = row.insertCell(1)
-    // initRollCell = row.insertCell(2)
-    for(char of orderedInit){
-        row = initTable.insertRow(0)
-        nameCell = row.insertCell(0)
-        charTypeCell = row.insertCell(1)
-        initRollCell = row.insertCell(2)
-        
-        nameCell.innerHTML = `${char.characterName}`
-        charTypeCell.innerHTML = `${char.characterType}`
-        initRollCell.innerHTML = `${char.initiativeRoll}`
-    }
+function buildInitTable(orderedInit) {
+	// row = table.insertRow(0)
+	// nameCell = row.insertCell(0)
+	// charTypeCell = row.insertCell(1)
+	// initRollCell = row.insertCell(2)
+	for (char of orderedInit) {
+		row = initTable.insertRow(0);
+		nameCell = row.insertCell(0);
+		charTypeCell = row.insertCell(1);
+		initRollCell = row.insertCell(2);
+
+		nameCell.innerHTML = `${char.characterName}`;
+		charTypeCell.innerHTML = `${char.characterType}`;
+		initRollCell.innerHTML = `${char.initiativeRoll}`;
+	}
 }
 
-function addToDamageList (){
-                // console.log("Add to Damage Tracker")
-                // console.log("Attack Successful: ", atkSuccess.value)
-                // console.log("Attack Type: ", atkType.value)
-                // console.log("Damage Inflicted: ", dmgInflicted.value)
-                // console.log("Damage Type: ", dmgType.value)
-                // console.log("Was Kill: ", killSuccess.value)
+function addToDamageList() {
+	// console.log("Add to Damage Tracker")
+	// console.log("Attack Successful: ", atkSuccess.value)
+	// console.log("Attack Type: ", atkType.value)
+	// console.log("Damage Inflicted: ", dmgInflicted.value)
+	// console.log("Damage Type: ", dmgType.value)
+	// console.log("Was Kill: ", killSuccess.value)
 
-    let dmgObj = {};
-    if (atkSuccess.value === "no"){
-        dmgObj.atkSuccess = atkSuccess.value;
-        dmgObj.atkType = "none";
-        dmgObj.dmg1Inflicted = 0;
-        dmgObj.dmg1Type = "none";
-        dmgObj.dmg2Inflicted = 0;
-        dmgObj.dmg2Type = "none";
-        dmgObj.killSuccess = "no";
-    } else {
-        dmgObj.atkSuccess = atkSuccess.value;
-        dmgObj.atkType = atkType.value;
-        dmgObj.dmg1Inflicted = dmg1Inflicted.value;
-        dmgObj.dmg1Type = dmg1Type.value;
-        dmgObj.dmg2Inflicted = dmg2Inflicted.value;
-        dmgObj.dmg2Type = dmg2Type.value;
-        dmgObj.killSuccess = killSuccess.value;
-    }
-	console.log(dmgObj)
+	let dmgObj = {};
+	if (atkSuccess.value === "no") {
+		dmgObj.atkSuccess = atkSuccess.value;
+		dmgObj.atkType = "none";
+		dmgObj.dmg1Inflicted = 0;
+		dmgObj.dmg1Type = "none";
+		dmgObj.dmg2Inflicted = 0;
+		dmgObj.dmg2Type = "none";
+		dmgObj.killSuccess = "no";
+	} else {
+		dmgObj.atkSuccess = atkSuccess.value;
+		dmgObj.atkType = atkType.value;
+		dmgObj.dmg1Inflicted = dmg1Inflicted.value;
+		dmgObj.dmg1Type = dmg1Type.value;
+		dmgObj.dmg2Inflicted = dmg2Inflicted.value;
+		dmgObj.dmg2Type = dmg2Type.value;
+		dmgObj.killSuccess = killSuccess.value;
+	}
+	console.log(dmgObj);
 	//push the obj into global variable
 	dmgList.push(dmgObj);
-    console.log ("full dmg lst:", dmgList)
+	console.log("full dmg lst:", dmgList);
 }
+
+function createDropDown(dataSet, listenter) {
+
+    for (index of dataSet) {
+        let option = document.createElement("option");
+		option.text = `${index.type}`;
+		listenter.add(option);
+	}
+}
+
+/***********
+ * ON LOAD *
+ ***********/
+
+createDropDown(ATTACKTYPES, atkTypeSelect);
+createDropDown(DAMAGETYPES, dmg1TypeSelect);
+createDropDown(DAMAGETYPES, dmg2TypeSelect);
