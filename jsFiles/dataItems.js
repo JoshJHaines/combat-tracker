90dc9cc34a3e453b08c80679cc73b3c6d8bb9ad4
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-30 10:56:32 -0400

    moved API info to it's own js file

--- jsFiles/dataItems.js
+++ jsFiles/dataItems.js
@@ -2,80 +2,10 @@ const resetAllButton = document.getElementById("resetAll");
 
 resetAllButton.addEventListener("click", resetALLdata);
 
-const APItoken = `rNcvKrCyAO_7NPzYh9ev56Q0UXSGul2ttWFRtMVfoOc`;
-const dogShitButton = document.getElementById("dogShit");
-const plantAPI = `https://trefle.io/api/v1/plants?${APItoken}`;
-
-const DnDapi = `https://www.dnd5eapi.co/api/races/`;
-const DnDmonstersAPI = `https://www.dnd5eapi.co/api/monsters/`;
-const DnDweaponAPI = "https://api.open5e.com/weapons/";
-
-dogShitButton.addEventListener("click", murlpe);
-
-function murlpe() {
-	fetch(DnDapi)
-		.then((response) => response.json())
-		.then((data) => {
-			// console.log(data.results)
-			// console.log(data.results[0].index)
-			for (i = 0; i < data.results.length; i++) {
-				raceOBJ = {
-					index: data.results[i].index,
-					name: data.results[i].name,
-				};
-				RACES.push(raceOBJ);
-			}
-		});
-	console.log("races   ", RACES);
-}
-
-function DnDmonsters() {
-	fetch(DnDmonstersAPI)
-		.then((response) => response.json())
-		.then((data) => {
-			//console.log(data.results)
-			// console.log(data.results[0].index)
-			for (i = 0; i < data.results.length-1; i++) {
-				fetch(DnDmonstersAPI + data.results[i].index)
-					.then((response) => response.json())
-					.then((data) => {
-						//console.log(data);
-						monsterOBJ = {
-							index: data.index,
-							name: data.name,
-							armor_class: data.armor_class,
-							hit_points: data.hit_points,
-						};
-						MONSTERS.push(monsterOBJ);
-					});
-			}
-		});
-	console.log("monsters   ", MONSTERS);
-}
-
-function DnDweapons() {
-	fetch(DnDweaponAPI)
-		.then((response) => response.json())
-		.then((data) => {
-			//console.log(data.results)
-			// console.log(data.results[0].index)
-			// for (i = 0; i < data.results.length; i++){
-			// 	raceOBJ = {
-			// 		index: data.results[i].index,
-			// 		name: data.results[i].name
-			// 	}
-			// 	RACES.push(raceOBJ)
-			// }
-		});
-}
 
 /*****************
  * STANDARD DATA *
  *****************/
-
-const RACES = [];
-
-const MONSTERS = [];
 
 const CHARACTERTYPES = [{ type: "pc" }, { type: "npc" }];
 
@@ -96,6 +26,10 @@ const DAMAGETYPES = [
 	{ type: "slashing" },
 	{ type: "thunder" },
 ];
+
+const RACES = [];
+
+const MONSTERS = [];
 
 /************************
  * INITIATIVE VARIABLES *
