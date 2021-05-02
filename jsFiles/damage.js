ba25fb24f98e982ebae366a38816e3a9c55f6de9
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-02 12:50:05 -0400

    cleaning up function name

--- jsFiles/damage.js
+++ jsFiles/damage.js
@@ -52,7 +52,7 @@ function addToDamageList() {
 			}
 		}
 	}
-	updatePlayerScoredata(dmgObj)
+	updatePlayerScoreData(dmgObj)
 	dmgList.push(dmgObj);
 	console.log("full dmg lst:", dmgList);
 	localStorage.setItem("dmgList", JSON.stringify(dmgList));
